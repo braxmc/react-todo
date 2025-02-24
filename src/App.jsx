6d07aee37f8c9f93bef2b398ffbaed6bf209fb1b
Braxton McClellan <thebraxmc@gmail.com>
2025-02-23 18:59:49 -0700

    collecting user input with refs and forwarded refs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,52 @@
-import ProjectSidebar from "./componenets/ProjectSidebar"
+import { useState } from 'react';
+
+import ProjectSidebar from "./componenets/ProjectSidebar.jsx"
+import NewProject from "./componenets/NewProject.jsx";
+import NoProjectSelected from "./componenets/NoProjectSelected.jsx";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectsState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      };
+
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      };
+    });
+  }
+
+  console.log(projectsState);
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
-    <main className="h-screen my-8">
-      <ProjectSidebar />
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
